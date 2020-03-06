49c16cd53a39b1d5946c6ddc1dbd2f08fc72187a
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-06 17:19:30 -0300

    The request response and next objects

--- main.ts
+++ main.ts
@@ -5,8 +5,26 @@ const server = restify.createServer({
     version: '1.0.0'
 });
 
-server.get('/hello', (req, resp, next) => {
-    resp.json({message: 'hello'});
+server.use(restify.plugins.queryParser());
+
+server.get('/info', (req, resp, next) => {
+    //Resp e Req
+    /*resp.contentType = 'application/json';
+    resp.status(400);
+    resp.setHeader('Content-Type', 'application/json');
+    resp.send({message: 'hello'});*/
+    resp.json({
+        browser: req.userAgent(),
+        method: req.method,
+        url: req.href(),
+        path: req.path(),
+        query: req.query,
+    });
+
+    //next
+    // Diz que a callback acabou
+    // Controlar o fluxo se tiver mais de uma callback configurado pra mesma rota
+    // Passar um objeto, ex: com status da resposta e mensagem
     return next();
 })
 
