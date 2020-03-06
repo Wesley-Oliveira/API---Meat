e2c7f8d95e78af52b6939d7469e2795793a331ea
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-06 20:31:31 -0300

    Mapping the Schema of a Document with Mongoose

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,23 +1,17 @@
-const users = [
-    {id: '1', name: 'Peter', email: 'peter@marvel.com'},
-    {id: '2', name: 'Bruce', email: 'bruce@dc.com'},
-];
+import * as mongoose from 'mongoose';
 
-export class User {
-    static findAll(): Promise<any[]>{
-        return Promise.resolve(users);
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+        select: false,
     }
+});
 
-    static findById(id: string): Promise<any>{
-        return new Promise(resolve => {
-            const filtered = users.filter(user => user.id === id);
-            let user = undefined;
-
-            if(filtered.length > 0) {
-                user = filtered[0];
-            }
-
-            resolve(user);
-        })
-    }
-}
+export const User = mongoose.model('User', userSchema)
