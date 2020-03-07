36208b8a9eced6fd50b250ddbe18d45b673bf4ba
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-07 00:12:34 -0300

    Default validators - Mongoose

--- users/users.model.ts
+++ users/users.model.ts
@@ -9,14 +9,25 @@ export interface User extends mongoose.Document {
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
+        required: true,
+        maxlength: 80,
+        minlength: 3,
     },
     email: {
         type: String,
+        required: true,
         unique: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     },
     password: {
         type: String,
+        required: true,
         select: false,
+    },
+    gender: {
+        type: String,
+        required: false,
+        enum: ['Male', 'Female'],
     }
 });
 
