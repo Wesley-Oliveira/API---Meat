be6ca6e9978706379f5dce99bf1264919268e431
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-09 16:24:31 -0300

    Custom Methods in Model - Mongoose

--- users/user.router.ts
+++ users/user.router.ts
@@ -11,9 +11,27 @@ class UsersRouter extends ModelRouter<User> {
             document.password = undefined;
         });
     }
+
+    findByEmail = (req, resp, next) => {
+        if(req.query.email){
+            User.findByEmail(req.query.email)
+                .then(user => {
+                    if(user) {
+                        return [user];
+                    } else {
+                        return [];
+                    }
+                })
+                .then(this.renderAll(resp, next))
+                .catch(next);
+        } else {
+            next();  
+        } 
+    }
     
     applyRoutes(application: restify.Server) {
-        application.get('/users', this.findAll);
+        application.get({path:'/users', version: '2.0.0'}, [this.findByEmail, this.findAll]);
+        //application.get({path:'/users', version: '1.0.0'}, this.findAll);
         application.get('/users/:id', [this.validateId, this.findById]);
         application.post('/users', this.save);
         application.put('/users/:id', [this.validateId, this.replace]);
