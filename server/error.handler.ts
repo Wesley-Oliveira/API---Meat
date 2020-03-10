ccb1ecfd74cb273c9212e456e4a49b841cb164c6
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-10 00:56:09 -0300

    Refactoring

--- server/error.handler.ts
+++ server/error.handler.ts
@@ -16,15 +16,14 @@ export const handleError = (req: restify.Request, resp: restify.Response, err, d
         case 'ValidationError':
             err.statusCode = 400;
             const messages: any[] = []
-
             for(let name in err.errors) {
                 messages.push({ message: err.errors[name].message });
             }
             err.toJSON = () => ({
+                message: 'Validation error while processing your request',
                 errors: messages
             });
             break;
     }
-
     done()
 }
