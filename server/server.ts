4f664a3661ada3d920be112593b67d4fb65fb7fe
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-11 10:56:28 -0300

    Enabling HTTPS

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as restify from 'restify';
 import * as mongoose from 'mongoose';
 import { environment } from '../common/environment';
@@ -23,7 +24,9 @@ export class Server{
                 
                 this.application = restify.createServer({
                     name: 'meat-api',
-                    version: '1.0.0'
+                    version: '1.0.0',
+                    certificate: fs.readFileSync('./security/keys/cert.pem'),
+                    key: fs.readFileSync('./security/keys/key.pem'),
                 });
                 
                 this.application.use(restify.plugins.queryParser());
