6a9287191dce7010738a5c8f2e90a1b73e8971b2
Wesley-Oliveira <wesley0leby@gmail.com>
2020-03-11 10:01:28 -0300

    Authentication with JWT

--- security/auth.handler.ts
+++ security/auth.handler.ts
@@ -1,6 +1,8 @@
 import * as restify from 'restify';
+import * as jwt from 'jsonwebtoken';
 import { NotAuthorizedError } from 'restify-errors';
 import { User } from '../users/users.model';
+import { environment } from '../common/environment';
 
 export const authenticate: restify.RequestHandler = (req, resp, next) => {
     const { email, password } = req.body
@@ -8,6 +10,10 @@ export const authenticate: restify.RequestHandler = (req, resp, next) => {
         .then(user => {
             if(user && user.matches(password)) {
                 // gerar o token
+                const token = jwt.sign({ sub: user.email, iss: 'meat-api' }, 
+                                        environment.security.apiSecret);
+                resp.json({ name: user.name, email: user.email, acessToken: token})
+                return next(false);
             } else {
                 return next( new NotAuthorizedError('Invalid Credentials'));
             }
